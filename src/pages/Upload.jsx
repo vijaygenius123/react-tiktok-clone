4acaa49bb71ae5fc0031e7fdef5c25e13b75a67f
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-08-16 18:33:39 +0100

    Initial Commit

---
+++ src/pages/Upload.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Upload.propTypes = {
+
+};
+
+function Upload(props) {
+    return (
+        <div>
+            <h1>Upload</h1>
+        </div>
+    );
+}
+
+export default Upload;
